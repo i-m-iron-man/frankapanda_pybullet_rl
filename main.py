158e1805fba9a0fb6ed0d255d0108eabb897d345
i-m-iron-man <50156704+i-m-iron-man@users.noreply.github.com>
2022-01-14 12:59:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ import numpy as np
 import ddpg.train as train
 import ddpg.buffer as buffer
 
-MAX_EPISODES = 5
+MAX_EPISODES = 50000
 MAX_STEPS = 500
 MAX_BUFFER = 1000000
 
-agent = Env()
-initial_state = agent.episode_reset()
+training_agent = Env(render=False)
+initial_state = training_agent.episode_reset()
     
 state_size = len(initial_state)
-action_size = agent.action_size
+action_size = training_agent.action_size
 action_lim = 10.0
 
 ram = buffer.MemoryBuffer(MAX_BUFFER)
@@ -28,7 +28,7 @@ trainer = train.Trainer(state_size, action_size, action_lim, ram)
 reward_list=[]
 
 for ep in range(MAX_EPISODES):
-    state = np.float32(agent.episode_reset())
+    state = np.float32(training_agent.episode_reset())
     trainer.reset()
     total_reward = 0.0
     total_actor_loss = 0.0
@@ -37,7 +37,7 @@ for ep in range(MAX_EPISODES):
 
     for step in range(MAX_STEPS):
         action = trainer.get_exploration_action(state)
-        new_state, reward, done = agent.step(action , state)
+        new_state, reward, done = training_agent.step(action , state)
         total_reward += reward
         new_state = np.float32(new_state)
 
@@ -52,6 +52,7 @@ for ep in range(MAX_EPISODES):
 
         if done:
             total_steps = step+1
+            print("target reached")
             break
 
     gc.collect()
@@ -59,6 +60,26 @@ for ep in range(MAX_EPISODES):
     print("actor loss: ",total_actor_loss)
     print("critic loss: ",total_critic_loss)
     print("\n")
+    if ep%500==0 and ep != 0:
+        trainer.save_models(ep)
+        testing_agent = Env(render=True)
+        total_testing_reward = 0.0
+        state = np.float32(testing_agent.episode_reset())
+        for step in range(MAX_STEPS):
+            action = trainer.get_exploitation_action(state)
+            new_state, reward, done = testing_agent.step(action, state)
+            total_testing_reward += reward
+            new_state = np.float32(new_state)
+            state = new_state
+            if done:
+                break
+        print("total testing reward: ", total_testing_reward)
+        print("\n")
+        testing_agent.end()
+        gc.collect()
+
+
+
 
 
 
